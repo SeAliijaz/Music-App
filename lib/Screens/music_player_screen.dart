398d0b94da941f22f7834f308d39b6966e1d43ab
Ali ijaz <Se.Aliijaz@gmail.com>
2022-07-05 17:13:10 +0500

    UI Completed

--- lib/Screens/music_player_screen.dart
+++ lib/Screens/music_player_screen.dart
@@ -1,59 +1,156 @@
-// ignore_for_file: avoid_unnecessary_containers
-
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:music_app/Widgets/custom_card.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 
-class MusicPlayerScreen extends StatelessWidget {
+class MusicPlayerScreen extends StatefulWidget {
   const MusicPlayerScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MusicPlayerScreen> createState() => _MusicPlayerScreenState();
+}
+
+class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Column(
-        children: [
-          ///App Bar Row
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                SizedBox(
-                  height: 50,
-                  width: 50,
-                  child:
-                      CustomCard(child: Center(child: Icon(Icons.arrow_back))),
-                ),
-                Text(
-                  "Audio Player",
-                  style: GoogleFonts.getFont("Salsa",
-                      textStyle: TextStyle(fontSize: 20)),
-                ),
-                SizedBox(
+      body: SafeArea(
+        child: Column(
+          children: [
+            // back button and menu button
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  SizedBox(
                     height: 50,
                     width: 50,
-                    child: CustomCard(child: Center(child: Icon(Icons.menu)))),
-              ],
-            ),
-          ),
-
-          ///Album Cover
-
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: CustomCard(
-              child: Column(
-                children: [
-                  Image.asset(
-                    "images/cover_art.png",
+                    child: CustomCard(child: Icon(Icons.arrow_back)),
                   ),
-                  Text("Icons Will be displayed here")
+                  Text('P L A Y L I S T'),
+                  SizedBox(
+                    height: 50,
+                    width: 50,
+                    child: CustomCard(child: Icon(Icons.menu)),
+                  ),
                 ],
               ),
             ),
-          ),
-        ],
+
+            // cover art, artist name, song name
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: CustomCard(
+                child: Column(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.asset('images/cover_art.png'),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Kota The Friend',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                  color: Colors.grey.shade700,
+                                ),
+                              ),
+                              const Text(
+                                'Birdie',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 22,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const Icon(
+                            Icons.favorite,
+                            color: Colors.red,
+                            size: 32,
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+
+            // start time, shuffle button, repeat button, end time
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Text('0:00'),
+                  Icon(Icons.shuffle),
+                  Icon(Icons.repeat),
+                  Text('4:22')
+                ],
+              ),
+            ),
+
+            // linear bar
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: CustomCard(
+                child: LinearPercentIndicator(
+                  lineHeight: 10,
+                  percent: 0.8,
+                  progressColor: Colors.green,
+                  backgroundColor: Colors.transparent,
+                ),
+              ),
+            ),
+
+            // previous song, pause play, skip next song
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: SizedBox(
+                height: 80,
+                child: Row(
+                  children: const [
+                    Expanded(
+                      child: CustomCard(
+                          child: Icon(
+                        Icons.skip_previous,
+                        size: 32,
+                      )),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20.0),
+                        child: CustomCard(
+                            child: Icon(
+                          Icons.play_arrow,
+                          size: 32,
+                        )),
+                      ),
+                    ),
+                    Expanded(
+                      child: CustomCard(
+                          child: Icon(
+                        Icons.skip_next,
+                        size: 32,
+                      )),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
